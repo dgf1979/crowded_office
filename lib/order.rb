e49608222c6ff819dfb95bae2512492815d169b7
dgf1979 <drew@finstrom.us>
2015-05-20 11:49:23 -0700

    update order model with method to return the name of the date in addition to the stored number

--- lib/order.rb
+++ lib/order.rb
@@ -1,7 +1,12 @@
 class Order < ActiveRecord::Base
+  require 'date'
   belongs_to :company
   has_many :lines
   validates :company_id, presence: true
   validates :month_id, presence: true
   validates :month_id, :inclusion => { :in => 1..12 }
+
+  def month_name
+    return Date::MONTHNAMES[self.month_id]
+  end
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -8,4 +8,9 @@ describe(Order) do
     Order.create(company_id: 4, month_id: 2)
     expect(Order.all.count).to(eq(1))
   end
+
+  it('returns the month name of the stored month') do
+    order = Order.create(company_id: 4, month_id: 8)
+    expect(order.month_name).to(eq('August'))
+  end
 end
